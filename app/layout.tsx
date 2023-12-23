fc37fb35b8d2602c25a4f450c8c3123e2799dfc0
B GOKUL <gokul0880@gmail.com>
2023-12-23 13:05:23 +0530

    update Layout Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import 'react-datepicker/dist/react-datepicker.css' ;
+
 
 const inter = Inter({ subsets: ['latin'] })
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css' ;
 import moment from 'moment';
 
 const BookingForm = () => {
