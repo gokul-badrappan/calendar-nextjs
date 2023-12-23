487775c04e8a839b6d6fef3e9dbb97e5e82368bf
B GOKUL <gokul0880@gmail.com>
2023-12-23 12:31:25 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+'use client'
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
 
-export default function Home() {
+const BookingForm = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedUser, setSelectedUser] = useState<string>('');
+  const [selectedTime, setSelectedTime] = useState<string>('');
+  const [allowedDates, setAllowedDates] = useState<Record<string, number[]>>({
+    'Person A': [1, 7, 14, 21, 28],
+    'Person B': [2, 8, 15, 22, 29],
+    'Person C': [3, 9, 16, 23, 30], 
+  });
+  const [availableTimes, setAvailableTimes] = useState<Record<string, string[]>>({
+    'Person A': ['09:00', '10:00'],
+    'Person B': ['09:30', '10:30'],
+    'Person C': ['11:00', '12 :00'],
+  });
+
+
+  const formattedToday = `${selectedDate.getDate()}/${selectedDate.getMonth() + 1}/${selectedDate.getFullYear()}`;
+
+  const handleSubmit = () => {
+    if (selectedDate && selectedTime && selectedUser) {
+      alert('Booking Successful!')
+      console.log('Booking Data:', {
+        user: selectedUser,
+        date: formattedToday,
+        time: selectedTime,
+      });
+
+      // Add your fetch request here to send booking data to the API
+      // const response = await fetch('/api/bookings', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({
+      //     user: selectedUser,
+      //     date: formattedToday,
+      //     time: selectedTime, 
+      //   }),
+      // });
+
+      // const data = await response.json();
+      // console.log(data);
+      // alert('Booking Successful!');
+    }
+  };
+
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
+  };
+
+  const handleUserChange = (user: string) => {
+    setSelectedUser(user);
+    setSelectedTime(''); 
+  };
+
+  const handleTimeChange = (time: string) => {
+    setSelectedTime(time);
+  };
+
+  const isDisabledDate = (date: Date) => {
+    const personDates = allowedDates[selectedUser] || [];
+    return personDates.includes(date.getDate());
+  };
+
+  const getTimesDropdown = () => {
+    const times = availableTimes[selectedUser] || [];
+    return (
+      <select
+        value={selectedTime}
+        onChange={(e) => handleTimeChange(e.target.value)}
+      >
+        <option value="" disabled>
+          Select a time
+        </option>
+        {times.map((time) => (
+          <option key={time} value={time}>
+            {time}
+          </option>
+        ))}
+      </select>
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="max-w-4x1 mx-auto text-center">
+      <h1 className="mb-4">Appointify</h1>
+      <div className="w-full">
+        <label htmlFor="userDropdown">Select User:</label>
+        <select
+          id="userDropdown"
+          value={selectedUser}
+          onChange={(e) => handleUserChange(e.target.value)}
+        >
+          <option value="" disabled>
+            Select a user
+          </option>
+          <option value="Person A">Person A</option>
+          <option value="Person B">Person B</option>
+          <option value="Person C">Person C</option>
+        </select>
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          filterDate={isDisabledDate}
+          minDate={moment().toDate()}
+          inline
+        />  
+        {getTimesDropdown()}
       </div>
+      <button className="mt-4" type="button" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default BookingForm;
