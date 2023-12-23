6a51bf04fcd523a2128ad48d9f5d00a18ba427a0
B GOKUL <gokul0880@gmail.com>
2023-12-23 12:48:49 +0530

    newproject

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker.css' ;
 import moment from 'moment';
 
 const BookingForm = () => {
